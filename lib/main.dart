96ca2badaefb8aa13272675979fa24fb865220e1
Ovidio R. Calvet <calveto@gmail.com>
2018-08-02 22:58:29 -0400

    Add Bar with CustomPaint

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,22 @@ class ChartPage extends StatefulWidget {
 
 class ChartPageState extends State<ChartPage> {
   final random = new Random();
-  int dataSet;
-  
+  int dataSet = 50;
+
   void changeData() {
     setState(() {
       dataSet = random.nextInt(100);
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Center(
-        child: new Text('Data set: $dataSet'),
+        child: new CustomPaint(
+          size: new Size(200.0, 100.0),
+          painter: new BarChartPainter(dataSet.toDouble()),
+        ),
       ),
       floatingActionButton: new FloatingActionButton(
         child: new Icon(Icons.refresh),
@@ -33,4 +36,31 @@ class ChartPageState extends State<ChartPage> {
       ),
     );
   }
+}
+
+class BarChartPainter extends CustomPainter {
+  static const barWidth = 10.0;
+
+  BarChartPainter(this.barHeight);
+
+  final double barHeight;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint()
+      ..color = Colors.blue[400]
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(
+      new Rect.fromLTWH(
+        (size.width - barWidth) / 2.0,
+        size.height - barHeight,
+        barWidth,
+        barHeight,
+      ),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(BarChartPainter old) => barHeight != old.barHeight;
 }
